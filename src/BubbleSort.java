efe06d246b3113528fc661144aa479fa16f547f3
eric sali <ericsali@erics-MacBook-Pro-4.local>
2022-09-29 18:27:00 -0400

    read file

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 import java.util.Arrays;
 
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
 
 
 public class BubbleSort {
@@ -9,14 +11,46 @@ public class BubbleSort {
 
         int [] number = new int [10];
 
-        System.out.print("Random Numbers:");
-        for (int d = 0 ; d<number.length ; d++){
-            number[d] = g.nextInt(100)+1;
+        FileWriter myWriter;
+
+        System.out.println();
+
+
+        try {
+            myWriter = new FileWriter("random-integer.txt");
+            
+
+            for (int d = 0 ; d<number.length ; d++){
+                number[d] = g.nextInt(100)+1;
+
+                try {
+
+                    //myWriter.write("\r\n");
+                    myWriter.write(new Integer(number[d]).toString());  
+                    myWriter.write("\r\n");
+
+                    System.out.println(number[d]);
+
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                    }
+
+                }
+    
+            
+            myWriter.close();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
         }
 
         System.out.print("\nunsorted Numbers:"+Arrays.toString(number));
 
         System.out.print("\nSorted Numbers:"+Arrays.toString(sort(number)));
+
+        System.out.print("\n\n");
+
     }
 
     private BubbleSort() { }
